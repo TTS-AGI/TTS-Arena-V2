bf359b173fb98f6c4102f2f0eae36f376895c51d
mrfakename <me@mrfake.name>
2025-04-19 17:13:42 -0700

    fix

--- tts.py
+++ tts.py
@@ -136,7 +136,7 @@ def predict_tts(text, model):
     
     response_json = result.json()
     audio_data = response_json["audio_data"]
-    audio_type = response_json["audio_type"]
+    audio_type = response_json["mime_type"]
     
     # Save audio to a temporary file
     import tempfile
